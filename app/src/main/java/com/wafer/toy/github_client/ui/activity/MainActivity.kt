9c9d4300f39f799c0ac135e7712d2035562e1de9
Wafer Li <omyshokami@gmail.com>
2016-10-10 00:14:01 +0900

    Move activity to ui package

--- app/src/main/java/com/wafer/toy/github_client/ui/activity/MainActivity.kt
+++ app/src/main/java/com/wafer/toy/github_client/ui/activity/MainActivity.kt
@@ -1,7 +1,8 @@
-package com.wafer.toy.github_client
+package com.wafer.toy.github_client.ui.activity
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import com.wafer.toy.github_client.R
 
 class MainActivity : AppCompatActivity() {
 
