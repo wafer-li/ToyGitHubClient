18d1d046052803e3a64b45c5eab0f2d6b5da3ea1
Wafer Li <omyshokami@gmail.com>
2016-10-09 16:40:00 +0900

    Initialize project

---
+++ app/src/main/java/wafer/com/toygithubclient/MainActivity.kt
@@ -0,0 +1,12 @@
+package wafer.com.toygithubclient
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
