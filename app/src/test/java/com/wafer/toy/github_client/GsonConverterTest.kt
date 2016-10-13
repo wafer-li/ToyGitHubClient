5edc3377d968265846dd2ca3902a05e3b68f9012
Wafer Li <omyshokami@gmail.com>
2016-10-14 02:45:05 +0900

    Supplement the gson converter test

--- app/src/test/java/com/wafer/toy/github_client/GsonConverterTest.kt
+++ app/src/test/java/com/wafer/toy/github_client/GsonConverterTest.kt
@@ -1,7 +1,6 @@
 package com.wafer.toy.github_client
 
 import com.google.gson.FieldNamingPolicy
-import com.google.gson.Gson
 import com.google.gson.GsonBuilder
 import org.hamcrest.CoreMatchers.*
 import org.hamcrest.MatcherAssert.assertThat
@@ -14,10 +13,21 @@ import org.junit.Test
  */
 class GsonConverterTest {
 
+    private val gson = GsonBuilder()
+            .setFieldNamingPolicy(FieldNamingPolicy.LOWER_CASE_WITH_UNDERSCORES)
+            .create()
+
     /**
-     * Test Gson Converter behavior of validation error
+     * Test Gson Converter behavior of data class partly empty
      *
-     * Result: Test PASS.
+     * Test Class: [ValidationError]
+     * Empty field: [ValidationError.field] [ValidationError.resource]
+     *
+     * Assertion: The class is NOT NULL,
+     *            The class field which has data, is NOT NULL
+     *            The class field which don't have data, will be NULL
+     *
+     * Result: Test PASS
      *
      * The field which didn't appear in json string,
      * will be NULL
@@ -25,10 +35,6 @@ class GsonConverterTest {
      */
     @Test
     fun testGsonConvertPartlyEmpty() {
-        val gson: Gson = GsonBuilder()
-                .setFieldNamingPolicy(FieldNamingPolicy.LOWER_CASE_WITH_UNDERSCORES)
-                .create()
-
         val json: String = "{\"message\":\"error_test\", code:\"custom\"}"
 
         val validationError = gson.fromJson(json, ValidationError::class.java)
@@ -38,5 +44,48 @@ class GsonConverterTest {
         assertThat(validationError.message, not(nullValue()))
         assertThat(validationError.field, `is`(nullValue()))
         assertThat(validationError.resource, `is`(nullValue()))
+    }
+
+    /**
+     * Test Gson Converter with data totally empty
+     *
+     * Test Class: [ValidationError]
+     *
+     * Assertion: The data class is NOT NULL
+     *
+     * Result: PASS
+     *
+     * That means even the total data is empty,
+     * the class itself WON'T BE NULL
+     *
+     */
+    @Test
+    fun testGsonConverterTotallyEmpty() {
+        val json: String = "{}"
+
+        val validationError = gson.fromJson(json, ValidationError::class.java)
+
+        assertThat(validationError, not(nullValue()))
+    }
+
+    /**
+     * Test Gson Converter with NULL json string
+     *
+     * Test Class: [ValidationError]
+     *
+     * Assertion: The data class is NULL
+     *
+     * Result: PASS
+     *
+     * That means if the json string is NULL,
+     * the data class will be NULL
+     */
+    @Test
+    fun testGsonConverterNull() {
+        val json: String? = null
+
+        val validationError = gson.fromJson(json, ValidationError::class.java)
+
+        assertThat(validationError, `is`(nullValue()))
     }
 }
