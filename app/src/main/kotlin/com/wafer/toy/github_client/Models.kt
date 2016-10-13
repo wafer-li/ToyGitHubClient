002178eb13ecd4f0b68dc5db48a87aae78b1c6a0
Wafer Li <omyshokami@gmail.com>
2016-10-14 02:15:19 +0900

    Add validation error model

---
+++ app/src/main/kotlin/com/wafer/toy/github_client/Models.kt
@@ -0,0 +1,10 @@
+package com.wafer.toy.github_client
+
+/**
+ * The Models class
+ * Please put more info here.
+ * @author wafer
+ * @since 16/10/14 01:50
+ */
+
+data class ValidationError(val resource: String?, val field: String?, val code: String?, val message: String?)
