83ecf8a0f608f2de07af54561dcab580d704dc9c
Wafer Li <omyshokami@gmail.com>
2016-10-25 22:39:22 +0900

    Add constants

---
+++ app/src/main/kotlin/com/wafer/toy/github_client/constants/SharedPreferenceKey.kt
@@ -0,0 +1,10 @@
+package com.wafer.toy.github_client.constants
+
+/**
+ * The SharedPreferenceKey class
+ * Please put more info here.
+ * @author wafer
+ * @since 16/10/25 20:56
+ */
+
+const val OAUTH_KEY = "oauth_key"
