e059bcc2375f9af0ad3c4de7b2e14e9c020a476a
Wafer Li <omyshokami@gmail.com>
2016-10-25 20:03:04 +0900

    Better practice to remove the static context

--- app/src/main/kotlin/com/wafer/toy/github_client/ToyGitHubClientApplication.kt
+++ app/src/main/kotlin/com/wafer/toy/github_client/ToyGitHubClientApplication.kt
@@ -11,17 +11,8 @@ import android.content.res.Resources
  * @since 16/10/10 18:02
  */
 class ToyGitHubClientApplication : Application() {
-    companion object {
-        lateinit var appContext: Context
-        private set
-
-        lateinit var resource: Resources
-        private set
-    }
 
     override fun onCreate() {
         super.onCreate()
-        appContext = applicationContext
-        resource = resources
     }
 }
