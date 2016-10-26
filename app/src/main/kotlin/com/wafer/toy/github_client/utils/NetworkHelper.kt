e31ffa92656a5960e045601faf9afae6fe478455
Wafer Li <omyshokami@gmail.com>
2016-10-26 19:53:51 +0900

    Top level function will be better

--- app/src/main/kotlin/com/wafer/toy/github_client/utils/NetworkHelper.kt
+++ app/src/main/kotlin/com/wafer/toy/github_client/utils/NetworkHelper.kt
@@ -15,70 +15,68 @@ import retrofit2.Response
 import retrofit2.Retrofit
 
 /**
- * The NetworkHelper class
- * Please put more info here.
+ * The NetworkHelper
+ *
+ * Just some util method when interacting with network
+ *
  * @author wafer
  * @since 16/10/25 21:28
  */
 
-object NetworkHelper {
+fun getOAuthKey(context: Context): String? {
+    val preference: SharedPreferences =
+            context.getSharedPreferences(SHARED_PREFERENCE_KEY, Context.MODE_PRIVATE)
+
+    return preference.getString(OAUTH_KEY, null)
+}
 
 
-    fun getOAuthKey(context: Context): String? {
-        val preference: SharedPreferences =
-                context.getSharedPreferences(SHARED_PREFERENCE_KEY, Context.MODE_PRIVATE)
+fun addBasicAuthentication(username: String, password: String): Retrofit {
+    val client: OkHttpClient = ApiManager.client
+    val builder: OkHttpClient.Builder = client.newBuilder()
 
-        return preference.getString(OAUTH_KEY, null)
+    builder.authenticator { route, response ->
+        val credential: String = Credentials.basic(username, password)
+        response.request().newBuilder()
+                .removeHeader("Authorization")
+                .addHeader("Authorization", credential).build()
     }
 
+    ApiManager.changeClient(client)
+    return ApiManager.retrofit
+}
 
-    fun addBasicAuthentication(username: String, password: String): Retrofit {
-        val client: OkHttpClient = ApiManager.client
-        val builder: OkHttpClient.Builder = client.newBuilder()
 
-        builder.authenticator { route, response ->
-            val credential: String = Credentials.basic(username, password)
-            response.request().newBuilder()
-                    .removeHeader("Authorization")
-                    .addHeader("Authorization", credential).build()
-        }
+fun addOAuthAuthenticator(oAuthToken: String): Retrofit {
+    val client: OkHttpClient = ApiManager.client
+    val builder: OkHttpClient.Builder = client.newBuilder()
 
-        ApiManager.changeClient(client)
-        return ApiManager.retrofit
+    builder.authenticator { route, response ->
+        response.request().newBuilder()
+                .removeHeader("Authorization")
+                .addHeader("Authorization", "token" + oAuthToken)
+                .build()
     }
 
+    ApiManager.changeClient(client)
+    return ApiManager.retrofit
+}
 
-    fun addOAuthAuthenticator(oAuthToken: String): Retrofit {
-        val client: OkHttpClient = ApiManager.client
-        val builder: OkHttpClient.Builder = client.newBuilder()
+fun login(username: String, password: String,
+          twoFactorAuthorizationCode: String? = null,
+          callback: (response: Response<OAuthTokenResponse>?,
+                     t: Throwable?) -> Unit) {
 
-        builder.authenticator { route, response ->
-            response.request().newBuilder()
-                    .removeHeader("Authorization")
-                    .addHeader("Authorization", "token" + oAuthToken)
-                    .build()
-        }
+    addBasicAuthentication(username, password)
+    ApiManager.services.login(username, password, twoFactorAuthorizationCode, AUTH_REQUEST)
+            .enqueue(object : Callback<OAuthTokenResponse> {
+                override fun onResponse(call: Call<OAuthTokenResponse>?,
+                                        response: Response<OAuthTokenResponse>?) {
+                    callback(response, null)
+                }
 
-        ApiManager.changeClient(client)
-        return ApiManager.retrofit
-    }
-
-    fun login(username: String, password: String,
-              twoFactorAuthorizationCode: String? = null,
-              callback: (response: Response<OAuthTokenResponse>?,
-                         t: Throwable?) -> Unit) {
-
-        addBasicAuthentication(username, password)
-        ApiManager.services.login(username, password, twoFactorAuthorizationCode, AUTH_REQUEST)
-                .enqueue(object : Callback<OAuthTokenResponse> {
-                    override fun onResponse(call: Call<OAuthTokenResponse>?,
-                                            response: Response<OAuthTokenResponse>?) {
-                        callback(response, null)
-                    }
-
-                    override fun onFailure(call: Call<OAuthTokenResponse>?, t: Throwable?) {
-                        callback(null, t)
-                    }
-                })
-    }
+                override fun onFailure(call: Call<OAuthTokenResponse>?, t: Throwable?) {
+                    callback(null, t)
+                }
+            })
 }
