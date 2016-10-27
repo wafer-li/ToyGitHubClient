74d6cd27d0c4d00d70e7b6ea2c2000483f72a9e5
Wafer Li <omyshokami@gmail.com>
2016-10-27 18:45:01 +0900

    Fix example unit test

--- app/src/test/java/com/wafer/toy/github_client/ExampleUnitTest.kt
+++ app/src/test/java/com/wafer/toy/github_client/ExampleUnitTest.kt
@@ -1,21 +1,17 @@
 package com.wafer.toy.github_client
 
-import android.support.test.InstrumentationRegistry
-import android.support.test.runner.AndroidJUnit4
-import org.junit.Assert.assertEquals
 import org.junit.Test
-import org.junit.runner.RunWith
+
+import org.junit.Assert.*
 
 /**
- * Instrumentation test, which will execute on an Android device.
-
+ * Example local unit test, which will execute on the development machine (host).
  * @see [Testing documentation](http://d.android.com/tools/testing)
  */
-@RunWith(AndroidJUnit4::class)
 class ExampleUnitTest {
     @Test
     @Throws(Exception::class)
-    fun testPlus() {
-        assertEquals(1 + 1, 2)
+    fun addition_isCorrect() {
+        assertEquals(4, (2 + 2).toLong())
     }
 }
