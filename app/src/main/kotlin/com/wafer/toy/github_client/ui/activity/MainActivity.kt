1963fb6d5a170f053b9bf097b512f7cd9e45f9f2
Wafer Li <omyshokami@gmail.com>
2016-10-27 22:53:46 +0900

    Add bg for account header

--- app/src/main/kotlin/com/wafer/toy/github_client/ui/activity/MainActivity.kt
+++ app/src/main/kotlin/com/wafer/toy/github_client/ui/activity/MainActivity.kt
@@ -1,13 +1,45 @@
 package com.wafer.toy.github_client.ui.activity
 
+import com.mikepenz.materialdrawer.AccountHeader
+import com.mikepenz.materialdrawer.AccountHeaderBuilder
+import com.mikepenz.materialdrawer.Drawer
+import com.mikepenz.materialdrawer.DrawerBuilder
 import com.wafer.toy.github_client.R
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : BaseActivity() {
+
+    private val drawer: Drawer by lazy { initDrawer() }
+    private val accountHeader: AccountHeader by lazy { initAccountHeader() }
+
+
     override fun initView() {
+        initToolbar()
+        drawer
+    }
+
+    private fun initToolbar() {
+        setSupportActionBar(toolbar)
     }
 
     override fun getLayoutRes(): Int {
         return R.layout.activity_main
     }
+
+    private fun initDrawer(): Drawer {
+        return DrawerBuilder()
+                .withActivity(this)
+                .withToolbar(toolbar)
+                .withActionBarDrawerToggle(true)
+                .withAccountHeader(accountHeader)
+                .build()
+    }
+
+    private fun initAccountHeader(): AccountHeader {
+        return AccountHeaderBuilder()
+                .withActivity(this)
+                .withHeaderBackground(R.drawable.account_header_background)
+                .build()
+    }
+
 }
