c72e2355e9b4f4286c5269e198370aeee200881e
Wafer Li <omyshokami@gmail.com>
2016-11-07 19:04:08 +0900

    Splash Activity as launcher and main

---
+++ app/src/main/java/com/wafer/toy/github_client/constants/ActivityMode.kt
@@ -0,0 +1,14 @@
+package com.wafer.toy.github_client.constants
+
+/**
+ * The ActivityMode
+ *
+ * The first section is activity name
+ * The second is mode
+ *
+ * @author wafer
+ * @since 16/11/7 18:52
+ */
+
+const val MAIN_DASHBOARD: Int = 1
+const val MAIN_EXPLORER: Int = 2
