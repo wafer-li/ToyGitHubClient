6cd181c504af5715f03a52b43412f37cec737006
Wafer Li <omyshokami@gmail.com>
2016-11-07 18:42:38 +0900

    Add splash activity

---
+++ app/src/main/java/com/wafer/toy/github_client/ui/activity/SplashActivity.kt
@@ -0,0 +1,12 @@
+package com.wafer.toy.github_client.ui.activity
+
+import com.wafer.toy.github_client.R
+
+class SplashActivity : BaseActivity() {
+    override fun init() {
+    }
+
+    override fun getLayoutRes(): Int {
+        return R.layout.activity_splash
+    }
+}
