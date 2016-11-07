8684989d112c560fb85df82457ea341405a5d427
Wafer Li <omyshokami@gmail.com>
2016-11-07 19:04:08 +0900

    Splash Activity as launcher and main

--- app/src/main/java/com/wafer/toy/github_client/ui/activity/SplashActivity.kt
+++ app/src/main/java/com/wafer/toy/github_client/ui/activity/SplashActivity.kt
@@ -1,12 +1,28 @@
 package com.wafer.toy.github_client.ui.activity
 
+import android.os.Handler
 import com.wafer.toy.github_client.R
+import com.wafer.toy.github_client.constants.MAIN_DASHBOARD
+import com.wafer.toy.github_client.constants.MAIN_EXPLORER
+import com.wafer.toy.github_client.utils.getOAuthToken
 
 class SplashActivity : BaseActivity() {
     override fun init() {
+        Handler().postDelayed({ checkOAuthToken() }, 2000)
     }
 
     override fun getLayoutRes(): Int {
         return R.layout.activity_splash
+    }
+
+    private fun checkOAuthToken() {
+        val oAuthToken = getOAuthToken(this)
+
+        if (oAuthToken == "") {
+            goToActivity(MainActivity::class.java, flags = MAIN_EXPLORER)
+        }
+        else {
+            goToActivity(MainActivity::class.java, flags = MAIN_DASHBOARD)
+        }
     }
 }
