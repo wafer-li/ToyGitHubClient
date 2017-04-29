d3c2f47fcdd16183d41881e82324a905624e75db
Wafer Li <omyshokami@gmail.com>
2017-04-30 00:06:51 +0900

    Add Login activity

---
+++ app/src/main/kotlin/com/wafer/toy/githubclient/ui/activity/LoginActivity.kt
@@ -0,0 +1,13 @@
+package com.wafer.toy.githubclient.ui.activity
+
+import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
+import com.wafer.toy.githubclient.R
+
+class LoginActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_login)
+    }
+}
