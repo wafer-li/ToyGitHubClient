e904c24d8ebc6d6f954256b9d575be76734d1458
Wafer Li <omyshokami@gmail.com>
2017-04-30 04:55:54 +0900

    Change to Response wrapper to get header

--- app/src/main/kotlin/com/wafer/toy/githubclient/network/Api.kt
+++ app/src/main/kotlin/com/wafer/toy/githubclient/network/Api.kt
@@ -3,6 +3,7 @@ package com.wafer.toy.githubclient.network
 import com.wafer.toy.githubclient.model.network.Repo
 import com.wafer.toy.githubclient.model.network.SearchResponse
 import io.reactivex.Flowable
+import retrofit2.Response
 import retrofit2.http.GET
 import retrofit2.http.QueryMap
 import retrofit2.http.Url
@@ -16,7 +17,7 @@ import retrofit2.http.Url
 interface Api {
 
     @GET("/search/repositories")
-    fun getTrendingRepos(@QueryMap(encoded = true) params: Map<String, String>): Flowable<SearchResponse<Repo>>
+    fun getTrendingRepos(@QueryMap(encoded = true) params: Map<String, String>): Flowable<Response<SearchResponse<Repo>>>
 
-    fun getTrendingRepos(@Url url: String): Flowable<SearchResponse<Repo>>
+    fun getTrendingRepos(@Url url: String): Flowable<Response<SearchResponse<Repo>>>
 }
