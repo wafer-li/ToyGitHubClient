2d26560b423ac3f07a5c7521d61a3e5d710ac994
Wafer Li <omyshokami@gmail.com>
2017-04-29 16:31:48 +0900

    WIP: Using fragment

--- app/src/main/kotlin/com/wafer/toy/githubclient/ui/activity/MainActivity.kt
+++ app/src/main/kotlin/com/wafer/toy/githubclient/ui/activity/MainActivity.kt
@@ -1,11 +1,11 @@
-package com.wafer.toy.githubclient
+package com.wafer.toy.githubclient.ui.activity
 
 import android.os.Bundle
-import android.support.design.widget.Snackbar
 import android.support.v4.view.GravityCompat
 import android.support.v7.app.ActionBarDrawerToggle
 import android.support.v7.app.AppCompatActivity
 import android.view.MenuItem
+import com.wafer.toy.githubclient.R
 import kotlinx.android.synthetic.main.activity_main.*
 import kotlinx.android.synthetic.main.app_bar_main.*
 
@@ -14,12 +14,6 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        setSupportActionBar(toolbar)
-
-        fab.setOnClickListener({ view ->
-            Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                    .setAction("Action", null).show()
-        })
 
         val toggle = ActionBarDrawerToggle(
                 this, drawer, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close)
