8bb9269e5a097392e57756d6f385cfd3f9ce6bf0
Wafer Li <omyshokami@gmail.com>
2017-04-30 05:40:58 +0900

    WIP: Building NestedContentFragment

--- app/src/main/kotlin/com/wafer/toy/githubclient/ui/fragment/NestedContentFragment.kt
+++ app/src/main/kotlin/com/wafer/toy/githubclient/ui/fragment/NestedContentFragment.kt
@@ -6,6 +6,7 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import com.wafer.toy.githubclient.R
+import com.wafer.toy.githubclient.application.Constants
 
 /**
  * The NestedContentFragment class
@@ -15,9 +16,50 @@ import com.wafer.toy.githubclient.R
  */
 class NestedContentFragment : Fragment() {
 
+    private lateinit var pageTitle: String
+
+    private val mainTitles = resources.getStringArray(R.array.main_tab_titles)
+    private val trendingTitles = resources.getStringArray(R.array.trending_tab_titles)
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        pageTitle = arguments.getString(Constants.PAGE_TITLE)
+
+        proceedMainTitle(mainTitles.indexOf(pageTitle))
+        proceedMainTitle(trendingTitles.indexOf(pageTitle))
     }
+
+    private fun proceedMainTitle(index: Int) {
+        when (index) {
+            0 -> {
+                // Dashboard
+            }
+            1 -> {
+                // Repos
+            }
+
+            else -> return
+        }
+    }
+
+    private fun proceedTrendingTitle(index: Int) {
+        when (index) {
+            0 -> {
+                // Today
+            }
+
+            1 -> {
+                // This week
+            }
+
+            2 -> {
+                // This month
+            }
+
+            else -> return
+        }
+    }
+
 
     override fun onCreateView(inflater: LayoutInflater?, container: ViewGroup?, savedInstanceState: Bundle?): View? {
         val view = inflater?.inflate(R.layout.content_main, container, false)
