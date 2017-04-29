d9b44df9711095049340716d7e98a137d7b35474
Wafer Li <omyshokami@gmail.com>
2017-04-30 01:15:17 +0900

    WIP: Building Login

--- app/src/main/kotlin/com/wafer/toy/githubclient/ui/activity/LoginActivity.kt
+++ app/src/main/kotlin/com/wafer/toy/githubclient/ui/activity/LoginActivity.kt
@@ -1,13 +1,19 @@
 package com.wafer.toy.githubclient.ui.activity
 
 import android.os.Bundle
+import android.support.design.widget.Snackbar
 import android.support.v7.app.AppCompatActivity
 import com.wafer.toy.githubclient.R
+import kotlinx.android.synthetic.main.activity_login.*
 
 class LoginActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login)
+
+        login_toolbar.setNavigationOnClickListener { onBackPressed() }
+
+        login.setOnClickListener { Snackbar.make(it, "login", Snackbar.LENGTH_LONG).show() }
     }
 }
