d0e0d63433478e5b662a3dd3137ba3d1a30d3b8e
Wafer Li <omyshokami@gmail.com>
2017-04-29 22:20:14 +0900

    Add header image click listener

--- app/src/main/kotlin/com/wafer/toy/githubclient/ui/activity/MainActivity.kt
+++ app/src/main/kotlin/com/wafer/toy/githubclient/ui/activity/MainActivity.kt
@@ -2,6 +2,7 @@ package com.wafer.toy.githubclient.ui.activity
 
 import android.content.Context
 import android.os.Bundle
+import android.support.design.widget.Snackbar
 import android.support.v4.view.GravityCompat
 import android.support.v7.app.ActionBarDrawerToggle
 import android.support.v7.app.AppCompatActivity
@@ -12,6 +13,7 @@ import com.wafer.toy.githubclient.application.Constants
 import com.wafer.toy.githubclient.ui.adapter.MainPagerAdapter
 import kotlinx.android.synthetic.main.activity_main.*
 import kotlinx.android.synthetic.main.app_bar_main.*
+import kotlinx.android.synthetic.main.nav_header_main.view.*
 
 class MainActivity : AppCompatActivity() {
 
@@ -36,7 +38,10 @@ class MainActivity : AppCompatActivity() {
         toggle.syncState()
 
         nav_view_bottom.setNavigationItemSelectedListener { onNavigationItemSelected(it) }
-
+        nav_view.getHeaderView(0).imageView.setOnClickListener {
+            // TODO Login View
+            Snackbar.make(it, "clicked", Snackbar.LENGTH_LONG).show()
+        }
 
         val pref = getSharedPreferences(Constants.PREF_NAME, Context.MODE_PRIVATE)
 
