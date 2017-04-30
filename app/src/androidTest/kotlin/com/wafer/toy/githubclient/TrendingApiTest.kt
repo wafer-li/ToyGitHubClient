6a9b728c0bbb54e54f3109a4cb35715f691af4fa
Wafer Li <omyshokami@gmail.com>
2017-04-30 22:19:55 +0900

    Add trending API test

---
+++ app/src/androidTest/kotlin/com/wafer/toy/githubclient/TrendingApiTest.kt
@@ -0,0 +1,52 @@
+package com.wafer.toy.githubclient
+
+import android.content.Context
+import android.support.test.InstrumentationRegistry
+import android.support.test.runner.AndroidJUnit4
+import com.wafer.toy.githubclient.network.ApiManager
+import com.wafer.toy.githubclient.network.Trending
+import io.reactivex.android.schedulers.AndroidSchedulers
+import io.reactivex.schedulers.Schedulers
+import io.reactivex.subscribers.TestSubscriber
+import okhttp3.ResponseBody
+import org.hamcrest.CoreMatchers.`is`
+import org.junit.Assert.assertThat
+import org.junit.Before
+import org.junit.Test
+import org.junit.runner.RunWith
+import retrofit2.Response
+
+/**
+ * The TrendingApiTest
+ * Please put more info here.
+ * @author wafer
+ * @since 17/4/30 21:09
+ */
+@RunWith(AndroidJUnit4::class)
+class TrendingApiTest {
+
+    lateinit var trending: Trending
+    lateinit var context: Context
+
+    @Before
+    fun setUp() {
+        context = InstrumentationRegistry.getTargetContext()
+        trending = ApiManager.apply { init(context) }.createTrendingService(Trending::class.java)
+    }
+
+    @Test
+    fun testTrending() {
+
+        val testSubscriber = TestSubscriber<Response<ResponseBody>>()
+
+        trending.getTrending()
+                .subscribeOn(Schedulers.io())
+                .observeOn(AndroidSchedulers.mainThread())
+                .subscribe(testSubscriber)
+
+        testSubscriber.awaitTerminalEvent()
+
+        testSubscriber.assertComplete().assertNoErrors()
+        assertThat(testSubscriber.values().all { it.isSuccessful && it.body().string().contains("html") }, `is`(true))
+    }
+}
