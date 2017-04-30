17ebd19221bacf2534998d5fa26e085ab9b265b7
Wafer Li <omyshokami@gmail.com>
2017-05-01 07:14:47 +0900

    Check if isLoaded before load data again

--- app/src/main/kotlin/com/wafer/toy/githubclient/ui/fragment/TrendingContentFragment.kt
+++ app/src/main/kotlin/com/wafer/toy/githubclient/ui/fragment/TrendingContentFragment.kt
@@ -59,81 +59,83 @@ class TrendingContentFragment : Fragment() {
 
         val since = getSinceParam(trendingTitles.indexOf(pageTitle))
 
-        ApiManager.createTrendingService(Trending::class.java)
-                .getTrending(since = since)
-                .subscribeOn(Schedulers.io())
-                .observeOn(AndroidSchedulers.mainThread())
-                .filter { it.isSuccessful }
-                .flatMap {
-                    // Map the HTML source to Repos
-                    Observable.fromIterable(
-                            Jsoup.parse(it.body().string()).select("ol.repo-list").first().children())
-                }
-                .map {
-                    // `it` is the repo-list item, particularly <li>
-                    val repoAElement = it.select("h3 > a").first()
-                    val repoLink = repoAElement.attr("href")
+        if (!isLoaded) {
 
-                    val repoTitle = repoAElement.text()
-
-                    val description = it.select(".py-1 > p").first()?.ownText()
-                    val lang = it.select("""[itemprop="programmingLanguage"]""")
-                            .first()?.text() ?: getString(R.string.unknown)
-
-                    val stars = it.select("""a[href="$repoLink/stargazers"]""").first().text()
-                            .filter { it.isDigit() }.toInt()
-                    val forks = it.select("""a[href="$repoLink/network"]""").first().text()
-                            .filter { it.isDigit() }.toInt()
-
-                    val contributors = it.select("a[href=$repoLink/graphs/contributors]").first()
-                            .children()
-                            .map {
-                                // it is the contributor's avatar
-                                val userName = it.attr("alt").apply { drop(1) }
-                                val avatarUrl = it.attr("src")
-                                User(userName = userName, avatarUrl = avatarUrl)
-                            }
-
-                    val starsTimeInterval = it.select("span.float-right").first().text()
-                            .filterNot { it == ',' }
-
-                    val repo = Repo(
-                            fullName = repoTitle,
-                            name = repoTitle.split("/")[1],
-                            description = description,
-                            language = lang,
-                            stargazersCount = stars,
-                            forksCount = forks)
-
-                    TrendingCard(repo, contributors, starsTimeInterval)
-                }
-                .subscribe(object : Observer<TrendingCard> {
-                    override fun onSubscribe(d: Disposable?) {
-                        Log.d("Subscribe", "S!")
-
-                        if (!isLoaded)
-                            trendingCards.clear()
-
+            ApiManager.createTrendingService(Trending::class.java)
+                    .getTrending(since = since)
+                    .subscribeOn(Schedulers.io())
+                    .observeOn(AndroidSchedulers.mainThread())
+                    .filter { it.isSuccessful }
+                    .flatMap {
+                        // Map the HTML source to Repos
+                        Observable.fromIterable(
+                                Jsoup.parse(it.body().string()).select("ol.repo-list").first().children())
                     }
+                    .map {
+                        // `it` is the repo-list item, particularly <li>
+                        val repoAElement = it.select("h3 > a").first()
+                        val repoLink = repoAElement.attr("href")
 
-                    override fun onComplete() {
-                        Log.d("Complete", "C!")
-                        trendingContentAdapter.notifyDataSetChanged()
-                        isLoaded = true
+                        val repoTitle = repoAElement.text()
+
+                        val description = it.select(".py-1 > p").first()?.ownText()
+                        val lang = it.select("""[itemprop="programmingLanguage"]""")
+                                .first()?.text() ?: getString(R.string.unknown)
+
+                        val stars = it.select("""a[href="$repoLink/stargazers"]""").first().text()
+                                .filter { it.isDigit() }.toInt()
+                        val forks = it.select("""a[href="$repoLink/network"]""").first().text()
+                                .filter { it.isDigit() }.toInt()
+
+                        val contributors = it.select("a[href=$repoLink/graphs/contributors]").first()
+                                .children()
+                                .map {
+                                    // it is the contributor's avatar
+                                    val userName = it.attr("alt").apply { drop(1) }
+                                    val avatarUrl = it.attr("src")
+                                    User(userName = userName, avatarUrl = avatarUrl)
+                                }
+
+                        val starsTimeInterval = it.select("span.float-right").first().text()
+                                .filterNot { it == ',' }
+
+                        val repo = Repo(
+                                fullName = repoTitle,
+                                name = repoTitle.split("/")[1],
+                                description = description,
+                                language = lang,
+                                stargazersCount = stars,
+                                forksCount = forks)
+
+                        TrendingCard(repo, contributors, starsTimeInterval)
                     }
+                    .subscribe(object : Observer<TrendingCard> {
+                        override fun onSubscribe(d: Disposable?) {
+                            Log.d("Subscribe", "S!")
 
-                    override fun onNext(t: TrendingCard?) {
-                        Log.d("Next", "N!")
-                        if (t != null && !isLoaded) {
-                            trendingCards.add(t)
+                            if (!isLoaded)
+                                trendingCards.clear()
                         }
-                    }
 
-                    override fun onError(t: Throwable?) {
-                        Log.d("ERROR", "E!")
-                        t?.printStackTrace()
-                    }
-                })
+                        override fun onComplete() {
+                            Log.d("Complete", "C!")
+                            trendingContentAdapter.notifyDataSetChanged()
+                            isLoaded = true
+                        }
+
+                        override fun onNext(t: TrendingCard?) {
+                            Log.d("Next", "N!")
+                            if (t != null && !isLoaded) {
+                                trendingCards.add(t)
+                            }
+                        }
+
+                        override fun onError(t: Throwable?) {
+                            Log.d("ERROR", "E!")
+                            t?.printStackTrace()
+                        }
+                    })
+        }
     }
 
     private fun getSinceParam(index: Int): String =
