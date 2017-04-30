767398592e01b3af64c9484182b8bcb104b0523e
Wafer Li <omyshokami@gmail.com>
2017-04-30 17:25:58 +0900

    WIP: Add repo cards layout

---
+++ app/src/main/kotlin/com/wafer/toy/githubclient/ui/adapter/TrendingContentAdapter.kt
@@ -0,0 +1,10 @@
+package com.wafer.toy.githubclient.ui.adapter
+
+/**
+ * The TrendingContentAdapter
+ * Please put more info here.
+ * @author wafer
+ * @since 17/4/30 15:41
+ */
+
+class TrendingContentAdapter
